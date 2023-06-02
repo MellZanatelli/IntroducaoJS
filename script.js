cba0e98a15877c27de49ad95768bfb3835d0fa43
MellZanatelli <mell.zanatelli@gmail.com>
2023-06-02 08:18:21 -0300

    aula 02/06

--- script.js
+++ script.js
@@ -2,16 +2,60 @@ const listElement = document.querySelector("#app ul")
 const inputElement = document.querySelector("#app input")
 const buttonElement = document.querySelector("#app button")
 
-const tarefas = ["Estudar para prova", "Dormir cedo"]
+//const tarefas = ["Estudar para prova", "Dormir cedo"]
 
-for (const iterator of tarefas) {
-    const tarefaElement = document.createElement("li")
-    const tarefaText = document.createTextNode(iterator)
+const tarefas = JSON.parse(localStorage.getItem("list")) || []
 
-    tarefaElement.appendChild(tarefaText)
-    listElement.appendChild(tarefaElement)
+function renderTarefas() {
+    listElement.innerHTML = "";
+    for (const iterator of tarefas) {
+        const tarefaElement = document.createElement("li")
+        const tarefaText = document.createTextNode(iterator)
 
-    //console.log(iterator)
+        const linkElement = document.createElement("a")
+        linkElement.setAttribute("href","#")
+
+        const pos = tarefas.indexOf(iterator)
+        linkElement.setAttribute("onclick",`deleteTarefas(${pos})`)
+
+        const linkText = document.createTextNode("Excluir")
+        linkElement.appendChild(linkText)
+
+        tarefaElement.appendChild(tarefaText)
+        tarefaElement.appendChild(linkElement)
+        listElement.appendChild(tarefaElement)
+
+        console.log(iterator)
+    }
 }
+renderTarefas()
 
 //console.log(listElement, inputElement, buttonElement)
+
+// for (let i = 0; i < tarefas.length; i++) {
+//    const tarefaElement = document.createElement("li")
+//    const tarefaText = document.createTextNode(iterator)
+
+//    tarefaElement.appendChild(tarefaText)
+//    listElement.appendChild(tarefaElement)
+//}
+
+function addTarefas() {
+    const tarefaText = inputElement.value
+    tarefas.push(tarefaText)
+    inputElement.value=""
+    renderTarefas()
+    saveToStorage()
+}
+
+buttonElement.onclick = addTarefas
+
+function deleteTarefas(pos) {
+    tarefas.splice(pos,1)
+    renderTarefas()
+    saveToStorage()
+}
+
+function saveToStorage() {
+    localStorage.setItem("list",JSON.stringify(tarefas))
+}
